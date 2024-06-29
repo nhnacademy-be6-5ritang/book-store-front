33e54b3e069060073cda10a48bee4566edb1a8a7
dlrudgjs104 <dlrudgjs104@naver.com>
2024-06-30 00:10:12 +0900

    feat: 도서 상세정보 페이지 완성

--- src/main/java/com/nhnacademy/bookstorefront/config/WebConfig.java
+++ src/main/java/com/nhnacademy/bookstorefront/config/WebConfig.java
@@ -10,7 +10,5 @@ public class WebConfig implements WebMvcConfigurer {
 	public void addViewControllers(ViewControllerRegistry registry) {
 		registry.addViewController("/admin").setViewName("admin/admin-account");
 		registry.addViewController("/user").setViewName("user/user-account");
-		registry.addViewController("/product-details").setViewName("book/product-details");
-		registry.addViewController("/get-book").setViewName("book/get-book");
 	}
 }
