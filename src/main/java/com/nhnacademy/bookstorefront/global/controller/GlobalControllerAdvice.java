850f19ddd525c53879acd5b37e9f1d2b5d1f7ac5
dlrudgjs104 <dlrudgjs104@naver.com>
2024-07-01 14:38:53 +0900

    feat: 메인페이지 링크 수정

---
+++ src/main/java/com/nhnacademy/bookstorefront/global/controller/GlobalControllerAdvice.java
@@ -0,0 +1,21 @@
+package com.nhnacademy.bookstorefront.global.controller;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import com.nhnacademy.bookstorefront.category.service.CategoryService;
+
+import lombok.RequiredArgsConstructor;
+
+@ControllerAdvice
+@RequiredArgsConstructor
+public class GlobalControllerAdvice {
+	private final CategoryService categoryService;
+
+	@ModelAttribute
+	public void categories(Model model) {
+		// 여기서 카테고리 데이터를 로드하고 반환
+		model.addAttribute("categories", categoryService.getCategories());
+	}
+}
