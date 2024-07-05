fc10062163c070175bd062a0166cc8bb09cd89ab
kimdaun <fhdlwp15@naver.com>
2024-07-05 10:47:41 +0900

    fix : 배송지 수정

--- src/main/java/com/nhnacademy/bookstorefront/delivery/feignclient/DeliveryServiceClient.java
+++ src/main/java/com/nhnacademy/bookstorefront/delivery/feignclient/DeliveryServiceClient.java
@@ -5,7 +5,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -41,7 +40,7 @@ public interface DeliveryServiceClient {
 	@DeleteMapping("/{deliveryId}")
 	ResponseEntity<Void> deleteDelivery(@PathVariable Long deliveryId);
 
-	@GetMapping("/{deliveryId}/{orderId}/Order-Policy")
+	@PutMapping("/{deliveryId}/{orderId}/Order-Policy")
 	ResponseEntity<UpdateDeliveryAddOrderPolicyResponse> addOrderPolicy(@PathVariable Long deliveryId, @PathVariable Long orderId);
 
 }
