d56595688d6c246fe7a973bb089f1bb38a653066
dlrudgjs104 <dlrudgjs104@naver.com>
2024-07-08 23:25:39 +0900

    feat: 도서장바구니 생성 DTO Request 수정

--- src/main/java/com/nhnacademy/bookstorefront/bookcart/dto/request/CreateBookCartRequest.java
+++ src/main/java/com/nhnacademy/bookstorefront/bookcart/dto/request/CreateBookCartRequest.java
@@ -1,4 +1,6 @@
 package com.nhnacademy.bookstorefront.bookcart.dto.request;
 
-public record CreateBookCartRequest(int bookQuantity) {
+public record CreateBookCartRequest(
+	Long bookId,
+	int bookQuantity) {
 }
