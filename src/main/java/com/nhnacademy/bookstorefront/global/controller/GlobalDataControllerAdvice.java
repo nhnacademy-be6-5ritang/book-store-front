fe29ee629ca03f1e327005831ac78a85ef26f3de
kimdaun <fhdlwp15@naver.com>
2024-07-09 08:48:58 +0900

    refactor : 글로벌 코드 수정

--- src/main/java/com/nhnacademy/bookstorefront/global/controller/GlobalDataControllerAdvice.java
+++ src/main/java/com/nhnacademy/bookstorefront/global/controller/GlobalDataControllerAdvice.java
@@ -26,6 +26,6 @@ public class GlobalDataControllerAdvice {
 	@ModelAttribute
 	public void categories(Model model) {
 		// 여기서 카테고리 데이터를 로드하고 반환
-		model.addAttribute("categoriesDisplay", null);
+		model.addAttribute("categoriesDisplay", categoryService.getCategories());
 	}
 }
