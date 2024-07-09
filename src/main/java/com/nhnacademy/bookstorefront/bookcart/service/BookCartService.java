3509ca2f5c7e531dd115c9b5163738f7dda630a5
dlrudgjs104 <dlrudgjs104@naver.com>
2024-07-10 00:29:49 +0900

    feat: 서버 > 쿠키 전달 로직 추가

--- src/main/java/com/nhnacademy/bookstorefront/bookcart/service/BookCartService.java
+++ src/main/java/com/nhnacademy/bookstorefront/bookcart/service/BookCartService.java
@@ -8,6 +8,8 @@ import com.nhnacademy.bookstorefront.bookcart.dto.request.CreateBookCartRequest;
 import com.nhnacademy.bookstorefront.bookcart.dto.request.UpdateBookCartRequest;
 import com.nhnacademy.bookstorefront.bookcart.dto.response.GetBookCartResponse;
 
+import jakarta.servlet.http.HttpServletResponse;
+
 /**
  * @author 이경헌
  * BookCartService는 도서 장바구니와 관련된 비즈니스 로직을 처리하는 인터페이스입니다.
@@ -17,9 +19,10 @@ public interface BookCartService {
 	 * 특정 장바구니의 도서 목록을 조회합니다.
 	 *
 	 * @param cartId 조회할 장바구니의 ID
+	 * @param resp   HTTP 응답 객체로, 필요한 경우 쿠키를 설정을 위해 사용
 	 * @return 도서 목록이 포함된 리스트
 	 */
-	List<GetBookCartResponse> getBookCartsByCartId(Long cartId);
+	List<GetBookCartResponse> getBookCartsByCartId(Long cartId, HttpServletResponse resp);
 
 	/**
 	 * 새로운 도서를 장바구니에 추가합니다.
