6c4bdf981e9f83d1fce77dca51993eb797266139
dlrudgjs104 <dlrudgjs104@naver.com>
2024-07-18 15:23:54 +0900

    feat: log n crash 적용중

--- src/main/java/com/nhnacademy/bookstorefront/global/config/LogNCrashAppender.java
+++ src/main/java/com/nhnacademy/bookstorefront/global/config/LogNCrashAppender.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.client.RestTemplate;
 
 import ch.qos.logback.classic.spi.ILoggingEvent;
@@ -14,6 +15,9 @@ public class LogNCrashAppender extends AppenderBase<ILoggingEvent> {
 	private final RestTemplate restTemplate = new RestTemplate();
 	private static final Logger logger = LoggerFactory.getLogger(LogNCrashAppender.class);
 
+	@Value("${spring.profiles.active}")
+	String logSource;
+
 	@Override
 	protected void append(ILoggingEvent loggingEvent) {
 		Map<String, Object> logData = new HashMap<>();
@@ -21,9 +25,9 @@ public class LogNCrashAppender extends AppenderBase<ILoggingEvent> {
 		logData.put("projectVersion", "1.0.0");
 		logData.put("logVersion", "v2");
 		logData.put("body", loggingEvent.getFormattedMessage());
-		logData.put("logSource", "Http");
+		logData.put("logSource", logSource);
 		logData.put("logType", "log");
-		logData.put("host", "localhost");
+		logData.put("host", "5ritang-front");
 
 		String url = "https://api-logncrash.nhncloudservice.com/v2/log";
 
@@ -31,7 +35,7 @@ public class LogNCrashAppender extends AppenderBase<ILoggingEvent> {
 			restTemplate.postForEntity(url, logData, String.class);
 			logger.info("log send run");
 		} catch (Exception e) {
-			logger.error("Error occurred while sending log to external service", e);
+			logger.error("외부 서비스로 로그를 보내는 동안 에러가 발생했습니다.", e);
 		}
 	}
 }
