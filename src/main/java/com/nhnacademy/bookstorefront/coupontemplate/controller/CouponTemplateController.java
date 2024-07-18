67607ad1990e537ac227c70214f6095189f6d1af
kihoo-ni <gudcjf532@gmail.com>
2024-07-19 04:52:41 +0900

    design "쿠폰 마이페이지, 템플릿페이지, 발행페이지 수정"

--- src/main/java/com/nhnacademy/bookstorefront/coupontemplate/controller/CouponTemplateController.java
+++ src/main/java/com/nhnacademy/bookstorefront/coupontemplate/controller/CouponTemplateController.java
@@ -61,7 +61,7 @@ public class CouponTemplateController {
 	// 쿠폰발급페이지 페이징처리
 	@GetMapping("/issue")
 	public String getAllCouponTemplatesIssuePaging(@PageableDefault(page=1, size = 3)Pageable pageable,Model model) {
-		try{
+
 			Page<CouponTemplateResponseDTO> couponTemplates = couponTemplateService.getAllCouponTemplatesByUserPaging(pageable);
 		int blockLimit = 3;
 		int startPage = 1;
@@ -77,9 +77,7 @@ public class CouponTemplateController {
 		model.addAttribute("coupontemplates", couponTemplates);
 		model.addAttribute("startPage", startPage);
 		model.addAttribute("endPage", endPage);
-		} catch (Exception e) {
-			model.addAttribute("error", "해당 쿠폰 템플릿 목록이 존재하지 않습니다");
-		}
+
 
 		return "coupon-user/user-coupon-issue";
 	}
