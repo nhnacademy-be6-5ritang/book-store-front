aa60e229b5ddd7f122a8e3ead4a32f1518231772
taehwan01 <monte6198@gmail.com>
2024-07-24 16:59:23 +0900

    feat: payco 로그인

--- src/main/resources/static/js/payco.js
+++ src/main/resources/static/js/payco.js
@@ -1,7 +1,7 @@
 const paycoLoginRequest = async () => {
     const clientId = '3RD5T_6SsRZZqiV8oXgma4_';
-    const redirectUri = 'http://local.host.com:8081/auth/payco/callback'; //TODO 컨트롤러에서 받도록 수정
-    // const redirectUri = 'https://5ritang.store/auth/payco/callback';
+    // const redirectUri = 'http://local.host.com:8081/auth/payco/callback'; //TODO 컨트롤러에서 받도록 수정
+    const redirectUri = 'https://5ritang.store/auth/payco/callback';
 
     const paycoAuthUrl = `https://id.payco.com/oauth2.0/authorize?`
         + `response_type=code`
