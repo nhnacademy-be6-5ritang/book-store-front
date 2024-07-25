29106a8b95b4b00be9db58407f9ab0a63e521522
dlrudgjs104 <dlrudgjs104@naver.com>
2024-07-25 12:45:55 +0900

    feat: 캐시 서비스 자바독 추가

--- src/main/java/com/nhnacademy/bookstorefront/cache/controller/CacheController.java
+++ src/main/java/com/nhnacademy/bookstorefront/cache/controller/CacheController.java
@@ -9,17 +9,31 @@ import com.nhnacademy.bookstorefront.cache.service.CacheService;
 
 import lombok.RequiredArgsConstructor;
 
+/**
+ * @author 이경헌
+ * 캐시 관리와 관련된 요청을 처리하는 컨트롤러 클래스입니다.
+ */
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/caches")
 public class CacheController {
 	private final CacheService cacheService;
 
+	/**
+	 * 캐시 관리 페이지를 반환합니다.
+	 *
+	 * @return 캐시 관리 페이지의 뷰
+	 */
 	@GetMapping
 	public String cacheManagePage() {
 		return "cache/main-cache";
 	}
 
+	/**
+	 * 메인 페이지의 캐시를 새로고침합니다.
+	 *
+	 * @return HTTP 200 OK 상태 코드를 담고 있는 {@link ResponseEntity}
+	 */
 	@GetMapping("/mainPage")
 	public ResponseEntity<Void> refreshCacheMainPage() {
 		cacheService.refreshMainPageCache();
