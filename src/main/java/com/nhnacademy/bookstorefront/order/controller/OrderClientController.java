bfea8959ea45874af40c76fee36469defda77b49
kimdaun <fhdlwp15@naver.com>
2024-07-31 09:50:51 +0900

    refactor : 로직 수정

--- src/main/java/com/nhnacademy/bookstorefront/order/controller/OrderClientController.java
+++ src/main/java/com/nhnacademy/bookstorefront/order/controller/OrderClientController.java
@@ -243,13 +243,11 @@ public class OrderClientController {
 		}
 		deliveryServiceImpl.updateDeliveryAddOrder(deliveryId, createOrderResponse.orderId());
 		bookOrderServiceImpl.updateOrder(orderListId, createOrderResponse.orderId());
-
+		bookCartService.deleteAllBookCart(cartId);
 		if (createOrderRequest.orderPrice().equals(new BigDecimal("0.00"))) {
 			paymentServiceImpl.savePointSalePayment(createOrderResponse.infoId());
-			bookCartService.deleteAllBookCart(cartId);
 			return "redirect:/api/orders/complete/" + createOrderResponse.infoId();
 		}
-
 		return "redirect:/api/payments/" + createOrderResponse.infoId();
 	}
 
@@ -627,9 +625,9 @@ public class OrderClientController {
 			userAndCouponService.updateCouponAfterPayment(createOrderRequest.couponId());
 		}
 		deliveryServiceImpl.updateDeliveryAddOrder(deliveryId, getBookOrderResponses.getFirst().orderId());
+		bookCartService.deleteAllBookCart(cartId);
 		if (createOrderRequest.orderPrice().equals(new BigDecimal("0.00"))) {
 			paymentServiceImpl.savePointSalePayment(createOrderResponse.infoId());
-			bookCartService.deleteAllBookCart(cartId);
 			return "redirect:/api/orders/complete/" + createOrderResponse.infoId();
 		}
 		return "redirect:/api/payments/" + createOrderResponse.infoId();
